e7ea4059d348d3a8d2fd8ea8ae49c1b723de7114
Valera Feofanov <valerafeofanov264@gmail.com>
2021-08-11 23:02:42 +0300

    feat: write solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n).toString();
+    if (n.length == 1) {
+      return n;
+    }
+    return n[n.length - 1] + reverse(n.substr(0, n.length - 1));
 }
